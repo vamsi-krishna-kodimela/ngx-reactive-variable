a2e7e88f86bfed61277086a9854bb92f13d5b11e
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2024-05-03 08:30:12 +0000

    chore: Remove unused files and update library structure

--- projects/ngx-reactive-variable/src/public-api.ts
+++ projects/ngx-reactive-variable/src/public-api.ts
@@ -1,6 +1,2 @@
-/*
- * Public API Surface of ngx-reactive-variable
- */
 
-export * from './lib/ngx-reactive-variable.service';
-export * from './lib/ngx-reactive-variable.component';
+export * from './lib/reactive-variable.class';
